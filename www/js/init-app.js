c6237f56fe5867aa9f6f5abad3b1a36b0d1648af
xmnboy <xmnboy@yahoo.com>
2014-10-16 15:06:04 -0700

    updated to be consistent with new app templates

--- www/js/init-app.js
+++ www/js/init-app.js
@@ -6,7 +6,7 @@
 
 
 /*jslint browser:true, devel:true, white:true, vars:true */
-/*global $:false, intel:false, app:false, dev:false */
+/*global $:false, intel:false, app:false, dev:false, device:false, cordova:false */
 /*global moment:false, performance:false, UAParser:false */
 /*global initAccel:false, initCompass:false, initGeoLocate:false, updateDeviceInfo:false */
 /*global btnBeep:false, btnBark:false, btnAccel:false, btnVibrate:false, btnCompass:false */
@@ -20,32 +20,48 @@ window.app = window.app || {} ;         // there should only be one of these, bu
 app.uaParser = {} ;                     // for holding the user agent object
 
 
+// Set to "true" if you want the console.log messages to appear.
+
+app.LOG = true ;
+
+app.consoleLog = function() {           // only emits console.log messages if app.LOG != false
+    if( app.LOG ) {
+        var args = Array.prototype.slice.call(arguments, 0) ;
+        console.log.apply(console, args) ;
+    }
+} ;
+
+
+
+// App init point (runs on custom app.Ready event from init-dev.js).
+// Runs after underlying device native code and webview/browser is ready.
+// Where you should "kick off" your application by initializing app events, etc.
+
 app.initEvents = function() {
     "use strict" ;
     var fName = "app.initEvents():" ;
-    console.log(fName, "entry") ;
+    app.consoleLog(fName, "entry") ;
 
-// Main app starting point (what dev.onDeviceReady calls after system is ready).
+// Main app starting point (dev.onDeviceReady kicks off with "app.Ready" event).
 // Runs after underlying device native code and webview/browser is initialized.
 // Where you should "kick off" your application by initializing app events, etc.
 
-// NOTE: Customize this function to initialize your application.
-
-    // initialize third-party libraries and event handlers
+    // NOTE: initialize third-party libraries and event handlers
 
     app.uaParser = new UAParser() ;
 
-    // initialize application code
+    // NOTE: initialize your application code
 
     initAccel() ;
     initCompass() ;
     initGeoLocate() ;
     updateDeviceInfo() ;
 
-    // Initialize app event handlers.
-    // TODO: if( test for respective components before attaching event handlers )
-    // TODO: configure to work with both touch and click events (mouse + touch)
+    // NOTE: initialize your app event handlers
     // See main.js, cordova-acc.js and cordova-geo.js for event handlers.
+
+    // TODO: configure following to work with both touch and click events (mouse + touch)
+    // see http://msopentech.com/blog/2013/09/16/add-pinch-pointer-events-apache-cordova-phonegap-app/
 
     var el, evt ;
 
@@ -77,15 +93,17 @@ app.initEvents = function() {
     el = document.getElementById("id_btnGeoCoarse") ;
     el.addEventListener(evt, btnGeoCoarse, false) ;
 
-    // after init is all done is a good time to remove our splash screen
+    // NOTE: ...you can put other miscellaneous init stuff in this function...
+    // NOTE: ...and add whatever else you want to do now that the app has started...
 
-    app.hideSplashScreen() ;                // this is optional for your app
+    app.initDebug() ;           // just for debug, not required; keep it if you want it or get rid of it
+    app.hideSplashScreen() ;    // after init is good time to remove splash screen; using a splash screen is optional
 
     // app initialization is done
     // app event handlers are ready
     // exit to idle state and just wait for events...
 
-    console.log(fName, "exit") ;
+    app.consoleLog(fName, "exit") ;
 } ;
 document.addEventListener("app.Ready", app.initEvents, false) ;
 
@@ -97,7 +115,22 @@ document.addEventListener("app.Ready", app.initEvents, false) ;
 app.initDebug = function() {
     "use strict" ;
     var fName = "app.initDebug():" ;
-    console.log(fName, "entry") ;
+    app.consoleLog(fName, "entry") ;
+
+    if( window.device && device.cordova ) {                     // old Cordova 2.x version detection
+        app.consoleLog("device.version: " + device.cordova) ;   // print the cordova version string...
+        app.consoleLog("device.model: " + device.model) ;
+        app.consoleLog("device.platform: " + device.platform) ;
+        app.consoleLog("device.version: " + device.version) ;
+    }
+
+    if( window.cordova && cordova.version ) {                   // only works in Cordova 3.x
+        app.consoleLog("cordova.version: " + cordova.version) ; // print new Cordova 3.x version string...
+
+        if( cordova.require ) {                                 // print included cordova plugins
+            app.consoleLog(JSON.stringify(cordova.require('cordova/plugin_list').metadata, null, 1)) ;
+        }
+    }
 
     // Following is for demonstration.
     // find the "system ready" indicator on our display
@@ -128,25 +161,30 @@ app.initDebug = function() {
         failedElement.setAttribute('style', 'display:block;') ;
     }
 
-    console.log(fName, "exit") ;
+    app.consoleLog(fName, "exit") ;
 } ;
-document.addEventListener("app.Ready", app.initDebug, false) ;
 
 
 
-// This may or may not be required, depends on your app and plugin configuration.
+// Using a splash screen is optional. This function will not fail if none is present.
 // This is also a simple study in the art of multi-platform device API detection.
 
 app.hideSplashScreen = function() {
+    "use strict" ;
     var fName = "app.hideSplashScreen():" ;
-    console.log(fName, "entry") ;
+    app.consoleLog(fName, "entry") ;
 
-    if( navigator.splashscreen ) {                              // Cordova API detected
+    // see https://github.com/01org/appframework/blob/master/documentation/detail/%24.ui.launch.md
+    // Do the following if you disabled App Framework autolaunch (in index.html, for example)
+    // $.ui.launch() ;
+
+    if( navigator.splashscreen && navigator.splashscreen.hide ) {   // Cordova API detected
         navigator.splashscreen.hide() ;
     }
-    if( window.intel && intel.xdk && intel.xdk.device ) {       // Intel XDK API detected
-        intel.xdk.device.hideSplashScreen() ;
+    if( window.intel && intel.xdk && intel.xdk.device ) {           // Intel XDK device API detected, but...
+        if( intel.xdk.device.hideSplashScreen )                     // ...hideSplashScreen() is inside the base plugin
+            intel.xdk.device.hideSplashScreen() ;
     }
 
-    console.log(fName, "exit") ;
+    app.consoleLog(fName, "exit") ;
 } ;
